22de19938457a8ef7a71b951998fc107f1d8c54d
Iris Veress <68661896+VeressIris@users.noreply.github.com>
2023-11-17 22:51:49 +0200

    added scrollable text

--- app/src/main/java/com/example/unihack2023/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/unihack2023/ui/home/HomeFragment.kt
@@ -1,24 +1,23 @@
 package com.example.unihack2023.ui.home
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.unihack2023.LyricsSearchManager
+import com.example.unihack2023.R
+import com.example.unihack2023.WebsiteTextFetcher
 import com.example.unihack2023.databinding.FragmentHomeBinding
 import com.google.cloud.translate.Detection
 import com.google.cloud.translate.Translate.TranslateOption
 import com.google.cloud.translate.TranslateOptions
-import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import com.example.unihack2023.LyricsSearchManager
-import com.example.unihack2023.WebsiteTextFetcher
 import kotlinx.coroutines.*
+
 
 class HomeFragment : Fragment() {
 
@@ -28,7 +27,7 @@ class HomeFragment : Fragment() {
     private val GoogleAPIkey:String = "AIzaSyC6OOmcv32-NvpVqWm_6QXkwNflZu5HDN0"
     private val GeniusAPIkey:String = "G5eF63EO4TJaDTCDc_FqsUvQF8A9u6l_Ob9F3G-GIu7J2x6BojRJjplN2hPfNacA"
 
-    var songName:String = "wish you were gay"
+    var songName:String = "Cro - Easy"
     val lyricsSearchManager = LyricsSearchManager()
 
     private val networkScope = CoroutineScope(Dispatchers.IO)
@@ -43,11 +42,7 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            translateText("Hallo, Welt!", textView)
-        }
 
         return root
     }
@@ -55,9 +50,17 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mainText:TextView = view.findViewById(R.id.text_home)
+
         networkScope.launch {
-            searchLyrics()
+            val lyrics = translateText(searchLyrics())
+            withContext(Dispatchers.Main) {
+                mainText.text = lyrics
+            }
         }
+
+        mainText.setMovementMethod(ScrollingMovementMethod())
+
     }
 
     override fun onDestroyView() {
@@ -67,8 +70,8 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun translateText(text: String, textView: TextView) {
-        GlobalScope.launch(Dispatchers.IO) {
+    private suspend fun translateText(text: String?): String {
+        return withContext(Dispatchers.IO) {
             try {
                 val translate = TranslateOptions.newBuilder().setApiKey(GoogleAPIkey).build().service
                 val detection: Detection = translate.detect(text)
@@ -78,45 +81,36 @@ class HomeFragment : Fragment() {
                     TranslateOption.sourceLanguage(detectedLanguage),
                     TranslateOption.targetLanguage("en")
                 )
-
-                // Update the UI on the Main thread
-                withContext(Dispatchers.Main) {
-                    textView.text = translation.translatedText
-                }
+                translation.translatedText
             } catch (e: Exception) {
-                // Log the exception to help identify the issue
                 e.printStackTrace()
                 Log.e("Error", e.toString())
-
-                // Update the UI on the Main thread with error message
-                withContext(Dispatchers.Main) {
-                    textView.text = "Translation failed"
-                }
+                "Translation failed"
             }
         }
     }
 
-    private suspend fun searchLyrics() {
-        withContext(Dispatchers.Default) {
-            lyricsSearchManager.searchLyrics(songName, GeniusAPIkey) { results ->
-                if (results != null) {
-                    Log.i("LyricResult", results[0].url)
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val websiteTextFetcher = WebsiteTextFetcher()
-                        val websiteText = getLyricsFromSite(websiteTextFetcher.fetchTextFromUrl(results[0].url))
-                        Log.i("TEXT", websiteText)
 
-                        // Handle fetched text here or update UI
-                    }
+    private suspend fun searchLyrics(): String {
+        val deferred = CompletableDeferred<String>()
 
-
-                } else {
-                    // Handle error or no results
-                    Log.e("LyricsSearch", "Failed to fetch lyrics")
+        lyricsSearchManager.searchLyrics(songName, GeniusAPIkey) { results ->
+            if (results != null && results.isNotEmpty()) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val websiteTextFetcher = WebsiteTextFetcher()
+                    val websiteText =
+                        getLyricsFromSite(websiteTextFetcher.fetchTextFromUrl(results[0].url))
+                    deferred.complete(websiteText)
                 }
+            } else {
+                Log.e("LyricsSearch", "Failed to fetch lyrics")
+                deferred.complete("Failed to fetch Lyrics")
             }
         }
+
+        return deferred.await()
     }
+
 
     fun getLyricsFromSite(text: String):String {
         val start: Int = text.indexOf('[')
